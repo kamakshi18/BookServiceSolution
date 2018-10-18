fb1f2b1a62f5d3d972c0e500f9a7545f7142f46c
kamakshi <kamakshi_neupane18@hotmail.com>
2018-10-18 10:58:26 +0200

    update Publisher model with constructors

--- BookService.API/Models/Publisher.cs
+++ BookService.API/Models/Publisher.cs
@@ -10,5 +10,18 @@ namespace BookService.WebAPI.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Country { get; set; }
+
+        public Publisher()
+        {
+
+        }
+        // with this constructor it is easir to type data in book service context
+        public Publisher(int id, string name, string country)
+        {
+            Id = id;
+            Name = name;
+            Country = country;
+        }
+
     }
 }
