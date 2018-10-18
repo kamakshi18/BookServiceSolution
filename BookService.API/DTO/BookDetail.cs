58c1be7587c0e9e6de604232b55b732d5c84a734
kamakshi <kamakshi_neupane18@hotmail.com>
2018-10-18 14:30:10 +0200

    bookdetail class

--- BookService.API/DTO/BookDetail.cs
+++ BookService.API/DTO/BookDetail.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿using BookService.WebAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BookService.WebAPI.Models
+namespace BookService.WebAPI.DTO
 {
-    public class Book
+    public class BookDetail
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string ISBN { get; set; }
-        [Display(Name="#")]
+        [Display(Name = "#")]
         public int NumberOfPages { get; set; }
-        public Author Author { get; set; }
-        public Publisher Publisher { get; set; }
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+        public int PublisherID { get; set; }
+        public string PublisherName { get; set; }
         public string FileName { get; set; }
         public int Year { get; set; }
         public Decimal Price { get; set; }
