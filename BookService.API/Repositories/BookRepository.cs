cc20eb33dd969f5297e9358c2a80b10409c55768
kamakshi <kamakshi_neupane18@hotmail.com>
2018-10-18 13:35:16 +0200

    Add BookRepository with list of books and  ist of bookbasic

---
+++ BookService.API/Repositories/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookService.WebAPI.Data;
+using BookService.WebAPI.DTO;
+using BookService.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Repositories
+{
+    public class BookRepository
+    {
+        private BookServiceContext bookServiceContext;
+
+        public BookRepository(BookServiceContext context)
+        {
+            bookServiceContext = context;
+        }
+
+        public List<Book> List()
+        {
+            return bookServiceContext.Books.ToList();
+        }
+
+        public List<BookBasic> ListBasic()
+        {
+            return bookServiceContext.Books.Select(
+                b=>new BookBasic
+                {
+                    Id = b.Id,
+                    Title = b.Title
+                }
+                ).ToList();
+        } 
+    }
+}
