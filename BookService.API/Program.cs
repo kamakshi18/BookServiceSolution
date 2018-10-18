8142b70cf738e019ed42cd5770790c0c46a79cfc
kamakshi <kamakshi_neupane18@hotmail.com>
2018-10-18 09:44:36 +0200

    classes

--- BookService.API/Program.cs
+++ BookService.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace BookService.API
+namespace BookService.WebAPI
 {
     public class Program
     {
