abc722a69826047217cbf7c9d62e9d66eab0ec63
kamakshi <kamakshi_neupane18@hotmail.com>
2018-10-18 16:22:35 +0200

    controller

--- BookService.API/Controllers/BooksController.cs
+++ BookService.API/Controllers/BooksController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BookService.WebAPI.DTO;
 using BookService.WebAPI.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -33,5 +35,29 @@ namespace BookService.WebAPI.Controllers
             return Ok(_bookRepository.ListBasic());
             
         }
+        [HttpGet("{id")]
+        public IActionResult GetBook(int id)
+        {
+            return Ok(_bookRepository.GetById(id));
+        }
+
+        // GET: api/books/imagebyname/book2.jpg
+        [HttpGet]
+        [Route("ImageByName/{filename}")]
+        public IActionResult ImageByFileName(string filename)
+        {
+            var image = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", filename);
+            return PhysicalFile(image, "image/jpeg");
+        }
+
+        // GET: api/books/imagebyid/6
+        [HttpGet]
+        [Route("ImageById/{bookid}")]
+        public IActionResult ImageById(int bookid)
+        {
+            BookDetail book = _bookRepository.GetById(bookid);
+            return ImageByFileName(book.FileName);
+        }
+
     }
 }
