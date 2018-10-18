4def995dfafae4ebabb7211219e5a7aed775b6bd
kamakshi <kamakshi_neupane18@hotmail.com>
2018-10-18 14:28:25 +0200

    made class author basic

---
+++ BookService.API/DTO/AuthorBasic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.DTO
+{
+    public class AuthorBasic
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0}{1}", FirstName, LastName);
+            }
+        }
+    }
+}
